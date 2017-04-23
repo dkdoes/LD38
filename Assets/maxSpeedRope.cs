ff4f76136416aa739141a51a8d0e14accac49fbc
dkdoes <david.kennedy.does@gmail.com>
2017-04-23 11:35:40 -0700

    maybe

---
+++ Assets/maxSpeedRope.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class maxSpeedRope : MonoBehaviour {
+    public float maxSpeed = 20f;
+    private Rigidbody rigidbody;
+    public float speed;
+    
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    void FixedUpdate()
+    {
+        
+        if(rigidbody.velocity.magnitude > maxSpeed){
+           rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+        }
+        speed = rigidbody.velocity.magnitude;
+        Debug.Log(rigidbody.velocity);
+        
+    }
+    
+}
