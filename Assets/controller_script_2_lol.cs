b52ae3a2f96d9d4bc5297fc15dcda3ba3cfa15c6
dkdoes <david.kennedy.does@gmail.com>
2017-04-24 10:06:04 -0700

    shooting and remove library

--- Assets/controller_script_2_lol.cs
+++ Assets/controller_script_2_lol.cs
@@ -6,6 +6,9 @@ public class controller_script_2_lol : MonoBehaviour {
     private float hasFired = 0f;
     public GameObject bullet;
 	public Camera camera;
+    public float bulletSpeed = 10;
+	public Vector3 direction0;
+	public Vector3 direction1;
 	// Use this for initialization
 	void Start () {
 		
@@ -19,10 +22,15 @@ public class controller_script_2_lol : MonoBehaviour {
                 Debug.Log("shoot");
                 var b = Instantiate(bullet);
                 b.transform.position = transform.position;
+				b.GetComponent<Rigidbody> ().velocity = direction0;
 				//b.transform.rotation = camera.transform.rotation;
-                var t = camera.transform.rotation.eulerAngles;
-                t.y = 1;
-                b.GetComponent<Rigidbody>().velocity = t;
+                /*
+                var t = camera.transform;
+                t.transform.rotation.x = 0;
+                t.transform.rotation.z = 0;
+                */
+				//var tt = t.forward * bulletSpeed;
+                //b.GetComponent<Rigidbody>().velocity = tt;
                 hasFired = 0.7f;
             }
         }
