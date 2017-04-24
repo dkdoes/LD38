726a35c795f5c52909e4d95532c5a7ae4fdfc48c
dkdoes <david.kennedy.does@gmail.com>
2017-04-24 10:19:16 -0700

    test

--- Assets/asteroidControl.cs
+++ Assets/asteroidControl.cs
@@ -2,18 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class asteroid : MonoBehaviour {
-    //public Vector3 direction;
-    
+public class asteroidControl : MonoBehaviour {
+
 	// Use this for initialization
 	void Start () {
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-    void FixedUpdate () {
 		
 	}
 }
